3ea02479ae99e566929f1248f0813ab7a12b17df
rohinvaidya <rohinvaidya@outlook.com>
2021-10-12 18:45:00 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from '../heroes/heroes.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
